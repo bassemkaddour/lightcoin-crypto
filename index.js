25a1efa93be2b2977c86386f625c932c07c25bdd
Bassem Kaddour <bassemkaddour@Bassems-MacBook-Pro.local>
2018-10-24 22:59:46 -0700

    Complete exercise

--- index.js
+++ index.js
@@ -1,29 +1,88 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0; 
+    for (const key of this.transactions) {
+      balance += key.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+class Transaction {
+  
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
+  }
+  
+  isAllowed(value, balance) {
+    return !(value < 0 && balance + value <= 0);
   }
 
   commit() {
-    balance -= this.amount;
+    if (this.isAllowed(this.value, this.account.balance)) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+    }
+  }
+  
+}
+
+class Withdrawal extends Transaction {
+  
+  get value () {
+    return -this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value () {
+    return this.amount;
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('myface'); 
+console.log('Starting balance: ', myAccount.balance)
+
+t1 = new Withdrawal(60, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
+console.log('Balance:', myAccount.balance)
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(10, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Transaction 2:', t2);
+console.log('Balance:', myAccount.balance)
+
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+console.log('Balance:', myAccount.balance)
+
+t4 = new Withdrawal(1000, myAccount);
+t4.commit();
+
+console.log('Transaction 4:', t4);
+console.log('Balance:', myAccount.balance)
